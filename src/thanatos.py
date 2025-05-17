bc8a21b9b718fe2df72c97de454602a09130e2ab
Swagatam <swagatamheroz@outlook.com>
2025-05-17 12:15:59 +0530

    chore: make methods static

--- src/thanatos.py
+++ src/thanatos.py
@@ -1,41 +1,43 @@
 from scanner.scanner import Scanner
-
 from sys import argv, exit
 
 
 class Thanatos:
-  def __init__(self):
-    self.had_error = False
+  _had_error = False
 
-  def main(self):
+  @staticmethod
+  def main():
     if len(argv) > 1:
       print("Usage: thanatos [script]")
       exit(64)
 
     if len(argv) == 1:
-      self._run_file(argv[0])
+      Thanatos._run_file(argv[0])
     else:
-      self._run_repl()
+      Thanatos._run_repl()
 
-  def _run_file(self, path: str):
+  @staticmethod
+  def _run_file(path: str):
     file_contents = None
     with open(path) as file:
       file_contents = file.read()
 
-    self._run(file_contents)
+    Thanatos._run(file_contents)
 
-    if self.had_error:
+    if Thanatos._had_error:
       exit(65)
 
-  def _run_repl(self):
+  @staticmethod
+  def _run_repl():
     while True:
       line = input("> ")
       if line == "": break
 
-      self._run(line)
-      self.had_error = False
+      Thanatos._run(line)
+      Thanatos._had_error = False
 
-  def _run(self, source: str):
+  @staticmethod
+  def _run(source: str):
     scanner = Scanner(source)
     tokens = scanner.scan_tokens()
 
@@ -43,14 +45,15 @@ class Thanatos:
       print(token)
 
   # implement better error handling later
-  def error(self, line: int, message: str):
-    self._report(line, "", message)
+  @staticmethod
+  def error(line: int, message: str):
+    Thanatos._report(line, "", message)
 
-  def _report(self, line: int, where: str, message: str):
+  @staticmethod
+  def _report(line: int, where: str, message: str):
     print(f"[line {line}] Error {where}: {message}")
-    self.had_error = True
+    Thanatos._had_error = True
 
 
 if __name__ == "__main__":
-  thanatos = Thanatos()
-  thanatos.main()
+  Thanatos.main()
