62b8c4730a23f3cc342528d4ca603e514f2b4af2
Swagatam <swagatamheroz@outlook.com>
2025-05-17 09:43:06 +0530

    chore: add bare-bones funtions for structuring code execution

---
+++ src/thanatos.py
@@ -0,0 +1,54 @@
+from scanner import Scanner
+from sys import argv, exit
+
+class Thanatos:
+  def __init__(self):
+    self.had_error = False
+
+  def main(self):
+    if len(argv) > 1:
+      print("Usage: thanatos [script]")
+      exit(64)
+
+    if len(argv) == 1:
+      self._run_file(argv[0])
+    else:
+      self._run_repl()
+
+  def _run_file(self, path: str):
+    file_contents = None
+    with open(path) as file:
+      file_contents = file.read()
+
+    self._run(file_contents)
+
+    if self.had_error:
+      exit(65)
+
+  def _run_repl(self):
+    while True:
+      line = input("> ")
+      if line == "": break
+
+      self._run(line)
+      self.had_error = False
+
+  def _run(self, source: str):
+    scanner = Scanner(source)
+    tokens = scanner.scan_tokens()
+
+    for token in tokens:
+      print(token)
+
+  # implement better error handling later
+  def error(self, line: int, message: str):
+    self._report(line, "", message)
+
+  def _report(self, line: int, where: str, message: str):
+    print(f"[line {line}] Error {where}: {message}")
+    self.had_error = True
+
+
+if __name__ == "__main__":
+  thanatos = Thanatos()
+  thanatos.main()
