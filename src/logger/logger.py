51af8bac537c5801f2c2e7db903fab69daa27fd4
Swagatam <swagatamheroz@outlook.com>
2025-05-19 16:50:57 +0530

    chore: improve error handling

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -1,9 +1,26 @@
+from scanner.token import Token
+from scanner.tokentype import TokenType
+from typing import Union
+
+
 class Logger:
   encountered_error = False
 
   @staticmethod
-  def error(line: int, message: str):
-    Logger._report(line, "", message)
+  def error(line_or_token: Union[int, Token], message: str):
+    line, token = None, None
+    if isinstance(line_or_token, Token):
+      token = line_or_token
+    else:
+      line = line_or_token
+
+    if token is not None:
+      if token.type == TokenType.EOF:
+        Logger._report(token.line, "at end", message)
+      else:
+        Logger._report(token.line, f"at '{token.lexeme}'", message)
+    elif line is not None:
+      Logger._report(line, "", message)
 
   @staticmethod
   def _report(line: int, where: str, message: str):
