c4094447edf95540eb17c5a55b52beaca6b248fb
Swagatam <swagatamheroz@outlook.com>
2025-05-19 09:53:24 +0530

    feat: implement syntax trees for expressions

---
+++ src/grammar/expr.py
@@ -0,0 +1,57 @@
+from abc import ABC, abstractmethod
+from scanner.token import Token
+from typing import Literal as Oneof, Optional, Protocol, TypeVar, Union
+
+
+ReturnType = TypeVar("ReturnType", covariant = True)
+
+
+class Expr(ABC):
+  @abstractmethod
+  def accept(self, visitor: "Visitor[ReturnType]") -> ReturnType:
+    ...
+
+
+class Visitor(Protocol[ReturnType]):
+  def visit_binary_expr(self, expr: "Binary") -> ReturnType: ...
+  def visit_grouping_expr(self, expr: "Grouping") -> ReturnType: ...
+  def visit_literal_expr(self, expr: "Literal") -> ReturnType: ...
+  def visit_unary_expr(self, expr: "Unary") -> ReturnType: ...
+
+
+class Binary(Expr):
+  def __init__(self, left: Expr, operator: Token, right: Expr):
+    self.left = left
+    self.operator = operator
+    self.right = right
+
+  def accept(self, visitor: Visitor[ReturnType]) -> ReturnType:
+    return visitor.visit_binary_expr(self)
+
+
+class Grouping(Expr):
+  def __init__(self, expression: Expr):
+    self.expression = expression
+
+  def accept(self, visitor: Visitor[ReturnType]) -> ReturnType:
+    return visitor.visit_grouping_expr(self)
+
+
+class Literal(Expr):
+  def __init__(
+    self,
+    value: Optional[Union[str, float, Oneof["true", "false", "void"]]]
+  ):
+    self.value = value
+
+  def accept(self, visitor: Visitor[ReturnType]) -> ReturnType:
+    return visitor.visit_literal_expr(self)
+
+
+class Unary(Expr):
+  def __init__(self, operator: Token, right: Expr):
+    self.operator = operator
+    self.right = right
+
+  def accept(self, visitor: Visitor[ReturnType]) -> ReturnType:
+    return visitor.visit_unary_expr(self)
