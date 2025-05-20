25bab78c95979db83ca70c7fcf4f6411ab593436
Swagatam <swagatamheroz@outlook.com>
2025-05-20 17:58:10 +0530

    feat: equip logger to report runtime errors

--- src/logger/logger.py
+++ src/logger/logger.py
@@ -1,3 +1,4 @@
+from errors.executionerror import ExecutionError
 from scanner.token import Token
 from scanner.tokentype import TokenType
 from typing import Union
@@ -5,6 +6,8 @@ from typing import Union
 
 class Logger:
   encountered_error = False
+  encountered_runtime_error = False
+
 
   @staticmethod
   def error(line_or_token: Union[int, Token], message: str):
@@ -21,6 +24,13 @@ class Logger:
         Logger._report(token.line, f"at '{token.lexeme}'", message)
     elif line is not None:
       Logger._report(line, "", message)
+
+
+  @staticmethod
+  def execution_error(error: ExecutionError):
+    print(f"{error.message}\n[line {error.token.line}]")
+    Logger.encountered_runtime_error = True
+
 
   @staticmethod
   def _report(line: int, where: str, message: str):
