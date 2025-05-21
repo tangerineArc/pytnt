4ed742760c7cc72aba292b8e33baec2126faa31b
Swagatam <swagatamheroz@outlook.com>
2025-05-21 17:15:54 +0530

    chore: enclose envs using cactus stack

--- src/interpreter/environment.py
+++ src/interpreter/environment.py
@@ -1,11 +1,12 @@
 from errors.executionerror import ExecutionError
 from scanner.token import Token
-from typing import Dict
+from typing import Dict, Optional
 
 
 class Environment:
-  def __init__(self):
+  def __init__(self, enclosing: Optional["Environment"] = None):
     self.values: Dict[str, object] = {}
+    self.enclosing = enclosing
 
   def define(self, name: str, value: object):
     self.values[name] = value
@@ -14,11 +15,18 @@ class Environment:
     if name.lexeme in self.values:
       return self.values[name.lexeme]
 
+    if self.enclosing != None:
+      return self.enclosing.get(name)
+
     raise ExecutionError(name, f"Undefined variable '{name.lexeme}'.")
 
   def assign(self, name: Token, value: object):
     if name.lexeme in self.values:
       self.values[name.lexeme] = value
+      return
+
+    if self.enclosing != None:
+      self.enclosing.assign(name, value)
       return
 
     raise ExecutionError(name, f"Undefined variable '{name.lexeme}'.")
