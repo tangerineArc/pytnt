68a01c27508cfd45fc5cca6810258960ae3f7de4
Swagatam <swagatamheroz@outlook.com>
2025-05-22 13:10:16 +0530

    feat: create function objects

--- src/interpreter/callable.py
+++ src/interpreter/callable.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from interpreter.environment import Environment
+from parser.stmt import Function
 from typing import List, TYPE_CHECKING
 
 if TYPE_CHECKING:
@@ -15,3 +17,30 @@ class Callable(ABC):
   @abstractmethod
   def arity(self) -> int:
     ...
+
+
+class FunctionObj(Callable):
+  def __init__(self, declaration: Function):
+    self.declaration = declaration
+
+
+  def call(
+    self, interpreter: "Interpreter", arguments: List[object]
+  ) -> object:
+    environment = Environment(interpreter.universe)
+    for i in range(len(self.declaration.params)):
+      environment.define(
+        self.declaration.params[i].lexeme, arguments[i]
+      )
+
+    interpreter.execute_block(self.declaration.body, environment)
+
+    return None
+
+
+  def arity(self) -> int:
+    return len(self.declaration.params)
+
+
+  def __repr__(self) -> str:
+    return f"<function {self.declaration.name.lexeme}>"
